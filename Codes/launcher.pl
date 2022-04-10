d35f28046eadc40e45d01955fabe48dece3827a9
Santiago Pérez <santiago.perez01@correo.usa.edu.co>
2022-04-09 21:42:30 -0500

    Updated launcher.pl

--- Codes/launcher.pl
+++ Codes/launcher.pl
@@ -7,7 +7,7 @@
 
 $PATH=`pwd`; # get the current path
 chomp($PATH); # remove the last character (\n)
-$N =36; # number of processes
+$N =10; # number of processes
 system "make clean"; # clean the previous compilation
 system "make all"; # compile the program
 
@@ -21,19 +21,21 @@ close($machineFile);
 #Vector of executables
 chomp($machineName);
 @Executables=("Algorithm1","Algorithm2","Algorithm3");
-@Dimension=("96","200","400","800","1600","3200");
-@Threads=("1","2","4","8");
+@Dimension=("100","200","400","600","800","1000","1200","1400","1600","1800","2000","2200","2400","2600","2800","3000");
 foreach $size (@Dimension){
 	foreach $exe (@Executables){
 		$File="results/results-$machineName.csv";
 		#print"$File \n";
-		open(FILE,">>",$File) or die "Could not open file $File";
-		print FILE "Node,Load,Algorithm,Time\n";
-		for($i=0;$i<$N;$i++){
-			print FILE "$machineName,$size,$exe,";
-			system "$PATH/$exe $size >> $File";
-			print FILE "\n";
-		}	
+		foreach $thread (@Threads){
+			open(FILE,">>",$File) or die "Could not open file $File";
+			print FILE "Node,NumThreads,Load,Algorithm,Time\n";
+			for($i=0;$i<$N;$i++){
+				print FILE "$machineName,$thread,$size,$exe,";
+				system "$PATH/$exe $size $thread>> $File";
+				print FILE "\n";
+			}
+		}
+			
 		close($File);
 	}
 	
